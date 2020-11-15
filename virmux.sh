344ad1292455fd4c1cc8c860bdb77aff0b336e0e
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2020-11-15 23:43:09 +0530

    Create virmux.sh

---
+++ virmux.sh
@@ -0,0 +1,59 @@
+clear
+printf '\033[36m';echo script by thelearn-tech
+printf '\033[32m';echo wellcome
+echo to
+printf '\033[33m';figlet Virmux
+sleep 2
+printf '\033[31m';echo your data is mine now
+sleep 1
+clear
+ifconfig
+echo
+printf '\033[31m';echo i know your ip know
+sleep 1
+printf '\033[31m';echo searching for gmail
+sleep 1
+echo 1 gmail found
+echo your gmail is *********@gmail.com
+echo your password is ££££££££££
+printf '\033[32m';echo got your gmail and password too
+sleep 3
+cd $HOME
+cd storage
+cd dcim
+ls
+cd ..
+cd music
+ls
+cd ..
+cd shared
+ls
+cd ..
+cd downloads
+ls
+cd ..
+cd movies
+ls
+cd ..
+cd pictures
+ls
+cd ..
+echo
+printf '\033[35m';echo uploding  above file and folder
+echo to my server
+printf '\033[32m';echo Thanks for giving me your data
+sleep 4
+clear
+printf '\033[36m';echo by the way
+printf '\033[36m';figlet only
+printf '\033[36m';figlet a
+printf '\033[36m';figlet prank
+sleep 3
+clear
+printf '\033[32m';echo if you got scare to death
+echo because of my script
+echo follow me on github
+echo https://www.github.com/thelearn-tech
+sleep 3
+printf '\033[36m';echo thanks for using script
+sleep 1
